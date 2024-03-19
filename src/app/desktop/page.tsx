9b2cfcac1517c6377db86bde76d178c9d5556ac4
Raymondkingjnr <raymondnnaji660@gmail.com>
2024-03-19 09:43:13 +0100

    new file:   next.config.js 	deleted:    next.config.mjs 	modified:   package-lock.json 	modified:   package.json 	new file:   public/manifest.json 	new file:   public/maskable_icon_x128.png 	new file:   public/maskable_icon_x192.png 	new file:   public/maskable_icon_x384.png 	new file:   public/maskable_icon_x512.png 	new file:   public/sw.js 	new file:   public/sw.js.map 	new file:   public/swe-worker-development.js 	new file:   public/workbox-7144475a.js 	new file:   public/workbox-7144475a.js.map 	new file:   src/app/desktop/page.tsx 	modified:   src/app/globals.css 	modified:   src/app/layout.tsx 	modified:   src/app/page.tsx

---
+++ src/app/desktop/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Desktop = () => {
+  return (
+    <div className=" pt-10 px-4 grid place-content-center place-items-center">
+      <h1 className=" text-center font-bold text-gray-900 text-base">
+        This website is only accessible from a mobile device owke!!
+      </h1>
+    </div>
+  );
+};
+
+export default Desktop;
